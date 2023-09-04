77ffd1b7b273045b2c256f0320cbafc903b24c88
LucaaDev <ulisesdraws@gmail.com>
2023-09-03 23:43:36 -0500

    removing the client-specific classes (not in use) and fixed the homepage button

--- src/client/kotlin/dev/lucaadev/ExampleModClient.kt
+++
@@ -1,9 +0,0 @@
-package dev.lucaadev
-
-import net.fabricmc.api.ClientModInitializer
-
-object ExampleModClient : ClientModInitializer {
-	override fun onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
-}
